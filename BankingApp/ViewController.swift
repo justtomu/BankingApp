0604b43e9cdda937f1ddd2747a2561e419bc5409
justtomu <s.bogdan007@yandex.ru>
2020-11-12 00:50:19 +0300

    Initial Commit

---
+++ BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Bogdan Skvorchevsky on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
