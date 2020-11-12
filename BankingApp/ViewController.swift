c4f065649a53f25bb4f7732bfdb758e39a8e1768
justtomu <s.bogdan007@yandex.ru>
2020-11-12 23:45:26 +0300

    added credit formula

--- BankingApp/ViewController.swift
+++ BankingApp/ViewController.swift
@@ -9,30 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var amount_label: UILabel!
+    @IBOutlet weak var AmountLabel: UILabel?
+    @IBOutlet weak var CreditAmountLabel: UILabel?
+    @IBOutlet weak var pvTextField: UITextField?
+    @IBOutlet weak var npeerTextField: UITextField?
+    @IBOutlet weak var rateTextField: UITextField!
     
     var amount: Double = 0 {
         didSet {
-            UpdeateAmountLabel()
+            UpdateAmountLabel()
         }
     }
     
-    func UpdeateAmountLabel(){
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func UpdateAmountLabel(){
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
         formatter.currencySymbol = "₽"
 //        let number = NSNumber(value: amount)
-        amount_label.text = formatter.string(from: NSNumber(value: amount))
+        AmountLabel?.text = formatter.string(from: NSNumber(value: amount))
+    }
+    
+    func UpdateCreditAmount(){
+        CreditAmountLabel?.text = nil
+        guard let pv = Double(pvTextField?.text ?? "") else { return }
+        guard let npeer = Double(npeerTextField?.text ?? "") else { return }
+        guard let rate = Double(rateTextField?.text ?? "") else { return }
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "₽"
+        
+//        let CreditAmount = pv + npeer + rate
+        let CreditAmount = abs(ExcelFormulas.pmt(rate: rate/100/12, nper: npeer, pv: pv))
+        CreditAmountLabel?.text = formatter.string(from: NSNumber(value: CreditAmount))
+//        CreditAmountLabel?.text = String(CreditAmount)
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        UpdeateAmountLabel()
+        UpdateCreditAmount()
+        UpdateAmountLabel()
         
-//        amount += 1.99
+        amount += 1.99
     }
 
-
+    @IBAction func TextFieldEdditingChange(_ sender: UITextField) {
+        UpdateCreditAmount()
+    }
+    
 }
 
